3ffacfa75a6222e80625e49540efff766c164afa
gaurav shitole <103493709+gauravshitole@users.noreply.github.com>
2023-11-30 11:12:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter.js'; // Make sure to adjust the path based on your file structure
+
+const App = () => {
+  return (
+    <div>
+      <h1>Slip 1</h1>
+      <Counter initialNumber={0} />
+    </div>
+  );
+};
+
+export default App;
---
+++ Counter.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const Counter = ({ initialNumber }) => {
+  const [count, setCount] = useState(initialNumber || 0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
